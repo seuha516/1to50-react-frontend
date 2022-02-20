d5eea2f1736c41fc7c2a254ae55a63cdc47e6044
SeungHa Jeon <seuha516@naver.com>
2022-02-20 17:30:34 +0900

    no message

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,17 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { shallowEqual, useSelector } from "react-redux";
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { shallowEqual, useSelector } from 'react-redux';
+import axios from 'axios';
 //components
-import Title from "./Title";
-import Target from "./Target";
-import Result from "./Result";
-import Time from "./Time";
-import Board from "./Board";
+import Title from './Title';
+import Target from './Target';
+import Result from './Result';
+import Time from './Time';
+import Board from './Board';
 //style
-import classNames from "classnames/bind";
-import styles from "../design/Home.module.scss";
+import classNames from 'classnames/bind';
+import styles from '../design/Home.module.scss';
 const cx = classNames.bind(styles);
-
-//서버 깨우기
-let repeatLimit = 2;
-const wakeUpServer = () => {
-  console.log("서버 가동 요청을 보냈습니다.");
-  axios
-    .get(`${process.env.REACT_APP_API_URL}/api/ranking/check`)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      if (repeatLimit > 0) {
-        console.log(`${repeatLimit}번 더 재시도합니다.`);
-        repeatLimit--;
-        wakeUpServer();
-      } else {
-        console.log("서버가 응답하지 않습니다.");
-      }
-    });
-};
 
 const Home = () => {
   //상태 받아오기
@@ -41,17 +20,15 @@ const Home = () => {
       gameState: data.gameState,
       rankLoading: data.rankLoading,
     }),
-    shallowEqual
+    shallowEqual,
   );
-  //서버 깨우기
-  useEffect(wakeUpServer, []);
   //return
   return (
-    <div className={cx("area")}>
+    <div className={cx('area')}>
       <Title />
-      <div className={cx("gameArea")}>
+      <div className={cx('gameArea')}>
         {(gameState === -2 || gameState === 0) && (
-          <div className={cx("info")}>
+          <div className={cx('info')}>
             {gameState === 0 ? <Target /> : <Result />}
             <Time />
           </div>
@@ -59,7 +36,7 @@ const Home = () => {
         <Board />
       </div>
       {gameState < 0 && !rankLoading && (
-        <Link className={cx("ranking")} to="/ranking">
+        <Link className={cx('ranking')} to="/ranking">
           Ranking
         </Link>
       )}
